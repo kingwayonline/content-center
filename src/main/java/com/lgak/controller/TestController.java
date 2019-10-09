ca4ca32878b59a872f67a2b40cbcf94333ecd03d
kingway <450578087@qq.com>
2019-10-09 17:05:00 +0800

    init

---
+++ src/main/java/com/lgak/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.lgak.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import service.TestService;
+
+
+@RestController
+public class TestController {
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private TestService testService;
+
+    @GetMapping("/test2")
+    public String test1() {
+        String body = restTemplate.getForEntity("http://user-center/test1", String.class).getBody();
+        return body;
+    }
+}
