ad2defdb4fbf14b149d9a1ae805e6902ccc70346
kingway <450578087@qq.com>
2019-10-11 14:10:02 +0800

    init

--- src/main/java/com/lgak/config/MyRequestOriginParser.java
+++ src/main/java/com/lgak/config/MyRequestOriginParser.java
@@ -2,15 +2,21 @@ package com.lgak.config;
 
 import com.alibaba.csp.sentinel.adapter.servlet.callback.RequestOriginParser;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Component
+//@Component
 public class MyRequestOriginParser implements RequestOriginParser {
     @Override
     public String parseOrigin(HttpServletRequest httpServletRequest) {
+        // 可以设置在header上
+        String origin = httpServletRequest.getParameter("origin");
+        if (StringUtils.isEmpty(origin)){
+            throw new IllegalArgumentException("origin必填");
+        }
 
         // 根据某个应用传过来的参数判断是哪个应用，从而设置sentinel的应用来源
-        return "foo";
+        return origin;
     }
 }
